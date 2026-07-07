1844aa6959b564cfed077d423d176a5270042dce
Gbrltarrz <cpe.gutierrez.gabrielluigi@gmail.com>
2026-07-07 19:05:06 +0800

    Install page Router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,21 @@
+import { Routes, Route } from 'react-router'
 import './App.css'
 import Header from './components/Header.jsx'
-import Offers from './components/offers.jsx'
-import Accommodations from './components/accommodations.jsx'
-import Testimonials from './components/testimonials.jsx'
-
-
-
-
+import Home from './pages/home.jsx'
+import FoodMenu from './pages/foodmenu.jsx'
+import SpaService from './pages/spaService.jsx'
+import MyBooking from './pages/mybooking.jsx'
 
 function App() {
-
   return (
     <>
-  
-      <Header>
-      </Header>
-      <Offers/>
-      <Accommodations/>
-      <Testimonials/>
-      
-      
-     
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<FoodMenu />} />
+        <Route path="/spa" element={<SpaService />} />
+        <Route path="/my-booking" element={<MyBooking />} />
+      </Routes>
     </>
   )
 }
---
+++ src/pages/spaService.jsx
@@ -0,0 +1,15 @@
+import './components/css/spaService.css'
+
+
+function SpaService() {
+    return (
+        <main className="page spa-service-page">
+            <h1 className="Spa">
+                Spa Service Page
+            </h1>
+            
+        </main>
+    )
+}
+
+export default SpaService
