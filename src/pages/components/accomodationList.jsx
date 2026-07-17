b21cb91ce1e2bdfe5904b6ee9f9c6f3b9b9ebedd
Gbrltarrz <cpe.gutierrez.gabrielluigi@gmail.com>
2026-07-17 13:21:36 +0800

    added some Data fetch Logic

--- src/pages/components/accomodationList.jsx
+++ src/pages/components/accomodationList.jsx
@@ -5,72 +5,7 @@ import {
     getNextAvailableDate,
     formatShortDate,
 } from '../../data/accommodationInventory.js'
-import houseSmall from '../../assets/temp/A-House-Small.png'
-import houseMedium from '../../assets/temp/A-House-Medium.png'
-import houseFamily from '../../assets/temp/A-House-Family.png'
-
-const list = [
-    {
-        id: 'table',
-        name: 'Table',
-        pax: '1-2 Pax',
-        minPax: 1,
-        maxPax: 2,
-        available: null,
-        price: null,
-        image: null,
-    },
-    {
-        id: 'tent',
-        name: 'Camping Tent',
-        pax: '1-3 Pax',
-        minPax: 1,
-        maxPax: 3,
-        available: null,
-        price: null,
-        image: null,
-    },
-    {
-        id: 'small',
-        name: 'A-House Small',
-        pax: '1-2 Pax',
-        minPax: 1,
-        maxPax: 2,
-        available: null,
-        price: null,
-        image: houseSmall,
-    },
-    {
-        id: 'medium',
-        name: 'A-House Medium',
-        pax: '2-4 Pax',
-        minPax: 2,
-        maxPax: 4,
-        available: null,
-        price: null,
-        image: houseMedium,
-    },
-    {
-        id: 'large',
-        name: 'A-House Family',
-        pax: '4-6 Pax',
-        minPax: 4,
-        maxPax: 6,
-        available: null,
-        price: null,
-        image: houseFamily,
-    },
-    {
-        id: 'pavilion',
-        name: 'Pavillion',
-        pax: '10-15 Pax',
-        minPax: 10,
-        maxPax: 15,
-        available: null,
-        price: null,
-        image: null,
-    }
-]
+import { accomodationOptions as list } from '../../data/accomodationOptions.js'
 
 export default function AccomodationList({ selectedAccomodation, onSelectAccomodation, checkIn, checkOut }){
     const trackRef = useRef(null)
@@ -165,5 +100,3 @@ export default function AccomodationList({ selectedAccomodation, onSelectAccomod
         </div>
     )
 }
-
-export { list as accomodationOptions }
